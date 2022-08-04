e8447fd1a5f808379ab8c7e7b03179f5832f9a79
CJ Kucera <cj@apocalyptech.com>
2022-08-04 13:48:29 -0500

    Adding a util to dump the pakfile DB to sqlite

---
+++ convert_pakfile_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# vim: set expandtab tabstop=4 shiftwidth=4:
+
+read -sp 'Enter bl3pakfile pass: ' PASS
+echo
+echo "SQLite dump/conversion..."
+echo
+rm bl3pakfile.sqlite3*
+/usr/local/dv/virtualenv/mysql2sqlite/bin/mysql2sqlite -f bl3pakfile.sqlite3 -d bl3pakfile -u bl3pakfile -h mcp -V -p ${PASS} && zip bl3pakfile.sqlite3.zip bl3pakfile.sqlite3 && rm bl3pakfile.sqlite3 && ls -lh bl3pakfile.sqlite3*
